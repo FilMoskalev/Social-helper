2748ff09ee2281a2cdf7c1985e3fd77b8ff58767
fil.moskalev <fil.moskalev@gmail.com>
2018-07-15 16:50:57 +0400

    setup

---
+++ Social Helper/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Social_Helper.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult Ru()
+    {
+      return View();
+    }
+
+    public ActionResult PrivacyPolicy()
+    {
+      return View();
+    }
+  }
+}
---
+++ Social Helper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Social_Helper.Startup))]
+namespace Social_Helper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
